437debfc52cd0e47ab953884e4d402927f11bed2
Emir Selimovic <emir.selimovichtl@gmail.com>
2020-01-12 14:28:17 +0100

    added clearbutton, gridNmbr between 0-100

--- script.js
+++ script.js
@@ -7,7 +7,15 @@
 const gridContainer = document.querySelector(".gameContainer")
 let gridContainerHeight = gridContainer.clientHeight;
 
+let color = "black";
+
+createGrid(16);
+
 function createGrid(gridNmbr) {
+    if (!isInputCorrect(gridNmbr)) {
+        createGrid(prompt("Please use numbers between 0 and 101"))
+        return;
+    }
     for (let x = 0; x < gridNmbr; x++) {
         let row = document.createElement("div");
         row.className = "row";
@@ -17,8 +25,23 @@ function createGrid(gridNmbr) {
             let cell = document.createElement("div");
             cell.className = "gridcell";
             row.appendChild(cell);
+            cell.addEventListener("mouseover", () => cell.style.background = color);
         }
         gridContainer.appendChild(row);
     }
+}
+const clearButton = document.querySelector(".clearButton");
+clearButton.addEventListener("click", clearGrid);
 
+function clearGrid() {
+    let allRows = document.querySelectorAll(".row");
+    allRows.forEach(row => {
+        gridContainer.removeChild(row);
+    });
+    createGrid(prompt("What size you want your grid?"));
+}
+
+function isInputCorrect(gridNmbr) {
+    if (isNaN(gridNmbr) || gridNmbr < 1 || gridNmbr > 100) return false;
+    return true;
 }
